2aa65550b4b617b18e3c4fe93b252768ff9dff4a
wundzunhome@wp.pl <wundzunhome@wp.pl@645648b6-66a5-5430-972e-ce039e707cca>
2011-01-01 12:04:58 +0000

    Logika działania testu samopoczucia.

---
+++ src/goodfeeling/util/Pair.java
@@ -0,0 +1,25 @@
+package goodfeeling.util;
+
+public class Pair<T, S> {
+
+    private final T first;
+    private final S second;
+
+    public Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair[first=" + first + ", second=" + second + "]";
+    }
+}
