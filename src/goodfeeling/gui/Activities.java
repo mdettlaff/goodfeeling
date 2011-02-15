ff10fb7eecfd3096d7c82f28c30b8e621967e429
pawel.olejniczak@gmail.com <pawel.olejniczak@gmail.com@645648b6-66a5-5430-972e-ce039e707cca>
2011-02-15 23:34:44 +0000

    Zapisywanie aktywności do bazy.

--- src/goodfeeling/gui/Activities.java
+++ src/goodfeeling/gui/Activities.java
@@ -1,5 +1,10 @@
 package goodfeeling.gui;
 
+import goodfeeling.common.AndroidFileIO;
+import goodfeeling.db.DbHandler;
+import goodfeeling.db.Record;
+import goodfeeling.db.RecordActivity;
+
 import java.util.Calendar;
 
 import android.app.Activity;
@@ -13,23 +18,16 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class Activities extends Activity {
-	String[] activities = { "spacer", "ryby", "rower", "basen", "łyżwy", "rolki",
-			"narty" };
+	String[] activities = { "spacer", "ryby", "rower", "basen", "łyżwy",
+			"rolki", "narty" };
 
-	int fromMin, fromHour, untilMin, untilHour;
-	String activity;
+	AutoCompleteTextView activityName;
+	TimePicker fromTimePicker, untilTimePicker;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activities);
-
-		Calendar calendar = Calendar.getInstance();
-
-		fromMin = calendar.getTime().getMinutes();
-		untilMin = calendar.getTime().getMinutes();
-		fromHour = calendar.getTime().getHours();
-		untilHour = calendar.getTime().getHours();
 
 		Button back = (Button) findViewById(R.id.back);
 		back.setOnClickListener(new View.OnClickListener() {
@@ -39,63 +37,59 @@ public class Activities extends Activity {
 			}
 		});
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_dropdown_item_1line, activities);
-
-		AutoCompleteTextView activityName = (AutoCompleteTextView) findViewById(R.id.activityName);
-		activityName.setThreshold(1);
-		activityName.setAdapter(adapter);
-		activityName
-				.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-					public void onItemClick(AdapterView<?> parent, View view,
-							int position, long id) {
-						activity = (String) parent.getItemAtPosition(position);
-					}
-				});
-
 		Button save = (Button) findViewById(R.id.save);
 		save.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				DisplayToast("W przyszłości po kliknięciu tego przycisku dane zostaną zapisane: "
-						+ activity
-						+ " od "
-						+ String.format("%02d", fromHour)
-						+ ':'
-						+ String.format("%02d", fromMin)
-						+ " do "
-						+ String.format("%02d", untilHour)
-						+ ':'
-						+ String.format("%02d", untilMin));
+				if (!save())
+					DisplayToast("Please fill all fields.");
 			}
 		});
 
-		TimePicker fromTimePicker = (TimePicker) findViewById(R.id.fromTimePicker);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line, activities);
+		activityName = (AutoCompleteTextView) findViewById(R.id.activityName);
+		activityName.setThreshold(1);
+		activityName.setAdapter(adapter);
+
+		fromTimePicker = (TimePicker) findViewById(R.id.fromTimePicker);
 		fromTimePicker.setIs24HourView(true);
-		fromTimePicker
-				.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
 
-					public void onTimeChanged(TimePicker view, int hourOfDay,
-							int minute) {
-						fromMin = minute;
-						fromHour = hourOfDay;
-					}
-				});
-
-		TimePicker untilTimePicker = (TimePicker) findViewById(R.id.untilTimePicker);
+		untilTimePicker = (TimePicker) findViewById(R.id.untilTimePicker);
 		untilTimePicker.setIs24HourView(true);
-		untilTimePicker
-				.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-
-					public void onTimeChanged(TimePicker view, int hourOfDay,
-							int minute) {
-						untilMin = minute;
-						untilHour = hourOfDay;
-					}
-				});
 	}
 
 	private void DisplayToast(String msg) {
 		Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+	}
+
+	private boolean save() {
+		String activityNameString = activityName.getText().toString(); 
+		System.out.println(activityNameString);
+		if (activityNameString.equals("")) return false;
+		
+		// TODO jeśli druga godzina jest mniejsza to jest fail
+		int duration = (untilTimePicker.getCurrentHour() - fromTimePicker
+				.getCurrentHour()) * 60	+ (untilTimePicker.getCurrentMinute()
+						- fromTimePicker.getCurrentMinute());
+		System.out.println(duration);
+		if (duration==0) return false;
+		
+		DbHandler dbHandler = new DbHandler(new AndroidFileIO(this));
+		try {
+			RecordActivity activity = new RecordActivity();
+			activity.name = activityNameString;
+			activity.startHour = fromTimePicker.getCurrentHour();
+			activity.duration = duration;
+			activity.intensivity = "Medium"; // ??
+
+			Record today = dbHandler.getRecord(Calendar.getInstance());
+			today.addActivity(activity);
+			dbHandler.addOrUpdateRecord(today);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
