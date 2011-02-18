9bcd60aef3f6e6b704281b3be2276ba391ca7b67
pawel.olejniczak@gmail.com <pawel.olejniczak@gmail.com@645648b6-66a5-5430-972e-ce039e707cca>
2011-02-18 21:46:43 +0000

    Poprawienie widoku sugestii. (Nie ma blokowania)

--- src/goodfeeling/gui/Suggestions.java
+++ src/goodfeeling/gui/Suggestions.java
@@ -8,8 +8,11 @@ import goodfeeling.db.DbHandler;
 import goodfeeling.weka.Rule;
 import goodfeeling.weka.RulesFinder;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.os.SystemClock;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,35 +29,13 @@ public class Suggestions extends Activity {
 
 		SuggestionsNote = (TextView)findViewById(R.id.SuggestionsNote);
 		SuggestionsContent = (TextView)findViewById(R.id.SuggestionsContent);
-		SuggestionsWait = (TextView)findViewById(R.id.SuggestionsWait);
-
-		
-		
 		
 		start = (Button) findViewById(R.id.start);
 		start.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				start.setVisibility(View.INVISIBLE);
-				SuggestionsNote.setVisibility(View.INVISIBLE);
-				SuggestionsWait.setVisibility(View.VISIBLE);
-				
-				// myślałem, że to zlikwiduje zwiechę podczas 
-				// obliczeń, ale się myliłem...
-				Thread t = new Thread() {
-				    public void run() {
-				    	Suggestions.result = getSuggestions();
-				    }
-				};
-				t.start();
-				try {
-					t.join();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				SuggestionsWait.setVisibility(View.INVISIBLE);
-				SuggestionsContent.setText(Suggestions.result);
-				
+				start.setVisibility(View.GONE);
+				SuggestionsNote.setVisibility(View.GONE);
+                showDialog(PROGRESS_DIALOG);
 			}
 		});
 	}
@@ -79,4 +60,55 @@ public class Suggestions extends Activity {
 	    System.out.println(msg);
 		return msg;
 	}
+	
+	
+
+    static final int PROGRESS_DIALOG = 0;
+    ProgressThread progressThread;
+    ProgressDialog progressDialog;
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case PROGRESS_DIALOG:
+            progressDialog = new ProgressDialog(Suggestions.this);
+            progressDialog.setMessage("Loading. Please wait.");
+            progressThread = new ProgressThread(handler);
+            progressThread.start();
+            return progressDialog;
+        default:
+            return null;
+        }
+    }
+
+    final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            int total = msg.getData().getInt("finished");
+            if (total==1) {
+            	dismissDialog(PROGRESS_DIALOG);
+            	String res = progressThread.results;
+				if (res=="")
+					SuggestionsContent.setText("No suggestions found. Try to add more data.");
+				else
+					SuggestionsContent.setText(res);
+            }
+        }
+    };
+
+    private class ProgressThread extends Thread {
+        Handler mHandler;
+        public String results = "";
+       
+        ProgressThread(Handler h) {
+            mHandler = h;
+        }
+       
+        public void run() {
+        	results = getSuggestions();
+            Message msg = mHandler.obtainMessage();
+            Bundle b = new Bundle();
+            b.putInt("finished", 1);
+            msg.setData(b);
+            mHandler.sendMessage(msg);
+                
+        }
+    }
 }
